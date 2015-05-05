ffd2ab2ae96e7044f2f07f788c3d8513d1cdbd3d
sleitch <sleitch@SLeitch-e6530.credera.com>
2015-05-05 10:48:23 -0500

    xxx

--- rest/rest-exec.sh
+++ rest/rest-exec.sh
@@ -4,13 +4,17 @@ URL=http://172.24.200.10:4243/containers/8f3578ee8ddc/exec
 
 echo "Exec..."
 
-DATA='{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true,\"Tty\": false, \"Cmd\": [ \"date\" ]}'
+DATA='{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true,"Tty": false, "Cmd": [ "date" ]}'
 
-
-curl --data DATA $URL
-
-#curl -X POST -H 'application/json' -d '$DATA' '$URL'
 
 
 #
 #   test_sshd = 8f3578ee8ddc
+
+
+ curl -H 'Content-Type:application/json' -X POST $URL --data $DATA -vv
+ 
+ ## WORKS
+ #
+ #curl -H 'Content-Type:application/json' -X POST 'http://172.24.200.10:4243/containers/8f3578ee8ddc/exec' --data '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true,"Tty": false, "Cmd": [ "date" ]}' -vv
+ 
