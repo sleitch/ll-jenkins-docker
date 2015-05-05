7316ca43b6fcc3c355b2edb0ea0b87ea94d8b4d5
sleitch <sleitch@SLeitch-e6530.credera.com>
2015-05-05 09:54:42 -0500

    ss

--- rest/rest-exec.sh
+++ rest/rest-exec.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-USERNAME=leisurelink+circleci
-PASSWORD=CLW67ZO3AYV414TS1JO9T2W7LZCYW7L14FSYJKH6OKZYYA6XACB5EM6OK58PUOQH
-SVRADDR=https://quay.io/v1/
 
 URL=http://172.24.200.10:4243/images/quay.io/leisurelink/authentic-api/push
 
-echo "Pushing..."
-#!/bin/bash
+
+echo "Exec..."
 
 DATA="{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true,\"Tty\": false, \"Cmd\": [ \"date\" ]}"
 
 
-curl -X POST -H "application/json" -d $DATA $URL
+curl -X POST -H 'application/json' -d $DATA $URL
 
 
 #
