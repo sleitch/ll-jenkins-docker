45d15ca7fcec90e69dd4c92120194fa13dc64e31
sleitch <sleitch@SLeitch-e6530.credera.com>
2015-05-05 16:37:42 -0500

    ss

--- rest/rest-exec-start.sh
+++ rest/rest-exec-start.sh
@@ -3,10 +3,15 @@
 #
 # The name (or ID) of the container to execute on
 #
-EXECID=4b161f9da73d71b41dbf86f287b94f232a270fc02ecf5ca1c1f07a1a52e2436b
 
 
+#
+# Pass this in from cmd arg
+#
+#EXECID=4b161f9da73d71b41dbf86f287b94f232a270fc02ecf5ca1c1f07a1a52e2436b
 
+
+echo "Arg 1 (is exec ID):  " $1
 
 #
 #  The data for the command:
@@ -14,7 +19,8 @@ EXECID=4b161f9da73d71b41dbf86f287b94f232a270fc02ecf5ca1c1f07a1a52e2436b
 DATA='{"Detach": false, "Tty": false}'
 
 
-URL='http://172.24.200.10:4243/exec/'"$EXECID"'/start'
+URL='http://172.24.200.10:4243/exec/'"$1"'/start'
+
 
 echo "Exec URL:  " $URL
 echo "Exec data: " $DATA
