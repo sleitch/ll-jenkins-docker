e8eebda3185d0ee3c5af9dbd6c797ed3ef0832b2
sleitch <sleitch@SLeitch-e6530.credera.com>
2015-05-06 14:49:00 -0500

    test hello script

---
+++ rest/exec-hello.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#
+# The name (or ID) of the container to execute on
+#
+CONTAINER=test_sshd
+
+#
+#  The data for the command:
+#
+DATA='{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true,"Tty": false, "Cmd": [ "hello.sh","testargs" ]}'
+
+
+URL='http://172.24.200.10:4243/containers/'"$CONTAINER"'/exec'
+
+echo "Exec URL:  " $URL
+echo "Exec data: " $DATA
+
+
+RES=$(curl -H 'Content-Type:application/json' -X POST $URL --data "$DATA")
+
+echo "Result=" $RES
+
+LEN=${#RES} 
+
+ ## Subtract the leading/trailing 9 chars - forthese guys:  {"Id":""}
+
+STRLEN=$(( $LEN - 9))
+
+CURRENT_EXEC_ID=${RES:7:$STRLEN}
+echo "substring: " ${RES:7:$STRLEN}
+#echo "CURRENT_EXEC_ID: " $CURRENT_EXEC_ID
+#export CURRENT_EXEC_ID
+
+echo "Arg 1 (is exec ID):  " $1
+
+#
+#  The data for the command:
+#
+DATA='{"Detach": false, "Tty": false}'
+
+
+URL='http://172.24.200.10:4243/exec/'"$CURRENT_EXEC_ID"'/start'
+
+
+echo "Exec URL:  " $URL
+echo "Exec data: " $DATA
+
+
+ STARTRES=$(curl -H 'Content-Type:application/json' -X POST $URL --data "$DATA" -v)
+ 
+ echo "Result of start exec: " $STARTRES
+ 
+ 
+
+
