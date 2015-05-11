39e6917ab5887b1998f8c00677143095439d3318
sleitch <sleitch@SLeitch-e6530.credera.com>
2015-05-11 16:00:46 -0500

    ss

--- rest/exec-deploy-test.sh
+++ rest/exec-deploy-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+############################################################################
 #
-# Define the execute start exec function for later use.
+#   Define the execute start exec function for later use.
+#   This will execute the start command against the exec ID
 #
-# This will execute the start command against the exec ID
-#
+############################################################################
 function startExec{ 
     #  The data for the command:
     #
@@ -22,96 +23,111 @@ function startExec{
     #STARTRES=$(curl -H 'Content-Type:application/json' -X POST $URL --data "$data" -v)
  
     echo "Result of start exec: " $STARTRES
-   
-   
-   
-   if [[ $STARTRET -ne 0 ]] ; then
+      
+    if [[ $STARTRET -ne 0 ]] ; then
         echo "Curl Error, exit code: $STARTRET"
     else
         #
         # This will split the result msg and code into array
         readarray -t startbody <<<"$STARTRET"
         
-          echo "startbody= "  ${startbody}
-                 
+        echo "startbody= "  ${startbody}                 
         echo "startbody 0 = "  ${startbody[0]}
-       echo "startbody 1 = "  ${startbody[1]}
+        echo "startbody 1 = "  ${startbody[1]}
         
-       # httpcode=${resultbody[2]}
-    
-      
+        # httpcode=${resultbody[2]}      
     fi
 }  
 
+
+
+############################################################################
+#
+#   Creates the exec instance ID for later use by start command.
+#
+############################################################################
+function createExec{ 
+   
+    CURRENT_EXEC_ID=""
+   
+    # this is json object for the exec create command..
+    #
+    data='{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true,"Tty": false, "Cmd": [ "deploytest.sh","'$1'", "'$2'" ]}'
+
+    URL='http://172.24.200.10:4243/containers/'"$CONTAINER"'/exec'
+
+    # get response and HTTP status code 
+    OUT=$( curl -qSfsw '\n%{http_code}'  -H 'Content-Type:application/json' -X POST $URL --data "$data") 2>/dev/null
+
+    # get exit code of curl
+    RET=$?
+    
+    echo "Exec URL:  " $URL
+    echo "Exec data: " $data
+    echo "Out= " $OUT
+    #echo "Curl exit code: " $RET
+    
+    if [[ $RET -ne 0 ]] ; then
+        echo "Curl Error, exit code: $RET"
+    else
+        #
+        # This will split the result msg and code into array
+        readarray -t resultbody <<<"$OUT"
+        httpcode=${resultbody[2]}
+    
+        echo "Curl Success, HTTP status is: " $httpcode
+        #echo "Response is: " ${resultbody[0]}
+     
+        if [[ $httpcode -eq 404 ]] ; then
+           # 404 no such container
+           echo "Error: 404,  No such container: " $CONTAINER
+        else
+    
+            if [[ $httpcode -ne 201 ]]; then
+               # if error exit code, print exit code
+               echo "HTTP Error Code (non 201):"  $httpcode
+            else
+               #
+               # Was ok...
+    
+               LEN=${#resultbody} 
+                
+                ## Subtract the leading/trailing 9 chars - forthese guys:  {"Id":""}
+                
+                STRLEN=$(( $LEN - 9))
+                
+                CURRENT_EXEC_ID=${resultbody[0]:7:$STRLEN}
+                echo "Successful HTTP status is: $httpcode"     
+                echo "Start EXEC ID: " $CURRENT_EXEC_ID
+            fi
+        fi
+    fi
+}  
+
+
+
+#
 # The name (or ID) of the container to execute on
 #
 CONTAINER=deploy-manager
 
-#
-#  The data for the command:
-#
-data='{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true,"Tty": false, "Cmd": [ "deploytest.sh","'$1'", "'$2'" ]}'
+############
+##
+##   First check if we have an existing EXEC ID to use:
+##
 
-URL='http://172.24.200.10:4243/containers/'"$CONTAINER"'/exec'
-
-# get response and HTTP status code 
-OUT=$( curl -qSfsw '\n%{http_code}'  -H 'Content-Type:application/json' -X POST $URL --data "$data") 2>/dev/null
-
-# get exit code of curl
-RET=$?
-
-echo "Exec URL:  " $URL
-echo "Exec data: " $data
-echo "Out= " $OUT
-#echo "Curl exit code: " $RET
-
-if [[ $RET -ne 0 ]] ; then
-    echo "Curl Error, exit code: $RET"
+if [ -z "$CURRENT_EXEC_ID" ]; then
+    echo "No env var called: CURRENT_EXEC_ID, creating an exec..."
+    
+    createExecID;
+    echo "exec ID after create: " $CURRENT_EXEC_ID
+    
+    startExec
+    
+    
 else
-    #
-    # This will split the result msg and code into array
-    readarray -t resultbody <<<"$OUT"
-    httpcode=${resultbody[2]}
-
-    echo "Curl Success, HTTP status is: " $httpcode
-    echo "Response is: " ${resultbody[0]}
- 
-    if [[ $httpcode -eq 404 ]] ; then
-       # 404 no such container
-       echo "Error: 404,  No such container: " $CONTAINER
-    else
-
-        if [[ $httpcode -ne 201 ]]; then
-           # if error exit code, print exit code
-           echo "HTTP Error Code (non 201):"  $httpcode
-        else
-           #
-           # Was ok...
-
-           LEN=${#resultbody} 
-			
-			## Subtract the leading/trailing 9 chars - forthese guys:  {"Id":""}
-			
-			STRLEN=$(( $LEN - 9))
-			
-			CURRENT_EXEC_ID=${resultbody[0]:7:$STRLEN}
-			
-			echo "Successful HTTP status is: $httpcode"		
-			#echo "resultbody =" ${resultbody[0]}
-			#echo "httpcode=" $httpcode
-			#echo "Len of out =  $LEN"
-			echo "Start EXEC ID: " $CURRENT_EXEC_ID
-			
-			
-			startExec
-        fi
-    fi
+    echo "Found CURRENT_EXEC_ID: " $CURRENT_EXEC_ID
+    
+    startExec
+    
 fi
- 
-
- 
- 
- 
-                
-
-
